cfd7e92572fdfb1635042069503f430af13a5022
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-02 15:56:01 +0300

    Initial Commit

---
+++ StudentTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentTasks
+//
+//  Created by mobileProg on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
