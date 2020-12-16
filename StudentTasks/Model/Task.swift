4a6b7c5c1efa0e0c05692e586651817c62b82ed5
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-16 14:14:44 +0300

    Refactor task model, add init and convenience init

--- StudentTasks/Model/Task.swift
+++ StudentTasks/Model/Task.swift
@@ -12,18 +12,20 @@ class Task: Codable, Equatable {
         lhs.id.uuidString == rhs.id.uuidString
     }
     
-    var id: UUID = UUID()
+    var id: UUID
     
     var name: String
     var description: String
-    
-    var course: Course
-    
-    var status: TaskStatus
+        
+    var status: TaskStatus {
+        return .ongoing // TOOD: Implement the logic
+    }
     var type: TaskType
     var priority: TaskPriority
     
-    var dueDate: Date?
+    var dueDate: Date
+
+    var course: Course?
     
     var completed: Bool
     var completedOn: Date?
@@ -32,6 +34,32 @@ class Task: Codable, Equatable {
     var gradeContribution: Float?
     var gradeType: TaskGradeType?
     var grade: Float?
+    
+    convenience init(name: String, description: String, type: TaskType, priority: TaskPriority, dueDate: Date) {
+        self.init(name: name, description: description, type: type, priority: priority, dueDate: dueDate, course: nil, completed: nil, completedOn: nil, graded: nil, gradeContribution: nil, gradeType: nil, grade: nil)
+    }
+    
+    init(name: String, description: String, type: TaskType, priority: TaskPriority, dueDate: Date, course: Course?, completed: Bool?, completedOn: Date?, graded: Bool?, gradeContribution: Float?, gradeType: TaskGradeType?, grade: Float?) {
+        
+        self.id = UUID() // Set universally unique identifier
+        
+        self.name = name
+        self.description = description
+        
+        self.type = type
+        self.priority = priority
+        self.dueDate = dueDate
+        
+        self.course = course ?? nil
+        
+        self.completed = completed ?? false
+        self.completedOn = completedOn ?? nil
+        
+        self.graded = graded ?? false
+        self.gradeContribution = gradeContribution ?? nil
+        self.gradeType = gradeType ?? nil
+        self.grade = grade ?? nil
+    }
 }
 
 enum TaskStatus: String, Codable {
