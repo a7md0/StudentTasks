e5a315afa29fce3283a38be1864a83e9862dc5fb
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-25 22:14:46 +0300

    Fix cells not swipeable

--- StudentTasks/Task list/TasksTableViewController.swift
+++ StudentTasks/Task list/TasksTableViewController.swift
@@ -113,7 +113,7 @@ class TasksTableViewController: UITableViewController {
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return false
+        return true
     }
 
     /*
