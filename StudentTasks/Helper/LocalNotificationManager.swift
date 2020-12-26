524f833fa0afd2c82bc123fb3ab7563f738dfd19
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-27 00:26:20 +0300

    Full refactor for notifications

--- StudentTasks/Helper/LocalNotificationManager.swift
+++ StudentTasks/Helper/LocalNotificationManager.swift
@@ -24,23 +24,54 @@ class LocalNotificationManager {
         }
     }
     
-    func scheduleNotification(notification: Notification) {
-        let trigger = UNCalendarNotificationTrigger(dateMatching: notification.dateMatching, repeats: false)
-        let request = UNNotificationRequest(identifier: notification.id.uuidString, content: notification.content, trigger: trigger)
-        
-        notificationCenter.add(request) { error in
-            guard error == nil else { return }
-            print("Scheduling notification with id: \(notification.id)")
+    func schedule(notification: Notification) {
+        schedule(notifications: [notification])
+    }
+    
+    func schedule(notifications: [Notification]) {
+        for notification in notifications {
+            let trigger = UNCalendarNotificationTrigger(dateMatching: notification.dateMatching, repeats: false)
+            let request = UNNotificationRequest(identifier: notification.id.uuidString, content: notification.content, trigger: trigger)
+            
+            notificationCenter.add(request) { error in
+                guard error == nil else { return }
+                print("Scheduling notification with id: \(notification.id)")
+            }
         }
     }
     
-    func descheduleNotification(identifier: UUID) {
-        self.descheduleNotifications(identifiers: [identifier])
+    private func deschedule(identifier: UUID) {
+        self.deschedule(identifiers: [identifier])
     }
     
-    func descheduleNotifications(identifiers: [UUID]) {
+    private func deschedule(identifiers: [UUID]) {
         let stringIdentifiers = identifiers.map { $0.uuidString }
         
         notificationCenter.removePendingNotificationRequests(withIdentifiers: stringIdentifiers)
+    }
+}
+
+extension LocalNotificationManager {
+    func prepareFor(task: Task) -> [UUID] {
+        let notifications: [Notification] = []
+
+        LocalNotificationManager.sharedInstance.schedule(notifications: notifications)
+        
+        return notifications.map { $0.id }
+    }
+    
+    func removeFor(task: Task) {
+        deschedule(identifiers: task.notifcationsIdentifiers)
+    }
+}
+
+struct Notification {
+    var id: UUID = UUID()
+    
+    var content = UNMutableNotificationContent()
+    var triggerDate: Date
+    
+    var dateMatching: DateComponents {
+        return Calendar.current.dateComponents([.year,.month,.day,.hour,.minute,.second,], from: self.triggerDate)
     }
 }
