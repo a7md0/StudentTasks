e3ffc6d8578d3efe53c625381df61715ef63997f
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-27 22:06:24 +0300

    Implement settings class/add notifications fields

---
+++ StudentTasks/Settings/SettingsTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsTableViewController.swift
+//  StudentTasks
+//
+//  Created by Ahmed Naser on 12/27/20.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
