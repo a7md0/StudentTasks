bc0ff41dfa6e2852f3515d24e0fe0f732504b08b
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-30 06:32:57 +0300

    Add notifications preferences view

---
+++ StudentTasks/Settings/NotificationsPreferencesTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationsPreferencesTableViewController.swift
+//  StudentTasks
+//
+//  Created by Ahmed Naser on 12/30/20.
+//
+
+import UIKit
+
+class NotificationsPreferencesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
