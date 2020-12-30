0bf3904476384976458cd18dddbbd008fe6fc7f0
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-30 08:51:44 +0300

    Settings update/Notification preference implement filters

--- StudentTasks/Settings/NotificationsPreferencesTableViewController.swift
+++ StudentTasks/Settings/NotificationsPreferencesTableViewController.swift
@@ -17,6 +17,10 @@ class NotificationsPreferencesTableViewController: UITableViewController {
     @IBOutlet weak var toDatePicker: UIDatePicker!
     var isToDatePickerHidden = true
     
+    
+    @IBOutlet weak var taskTypesLabel: UILabel!
+    @IBOutlet weak var taskPrioritiesLabel: UILabel!
+    
     let timeFormatter = DateFormatter()
     
     override func viewDidLoad() {
@@ -31,6 +35,7 @@ class NotificationsPreferencesTableViewController: UITableViewController {
         timeFormatter.timeStyle = .short
         
         updateDateLabels()
+        updateTasksFilters()
     }
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
@@ -42,17 +47,27 @@ class NotificationsPreferencesTableViewController: UITableViewController {
         toDateLabel.text = timeFormatter.string(from: toDatePicker.date)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateTasksFilters() {
+        
     }
-    */
-
+    
+    func handleTaskTypesPicker(items: [PickerItem]) {
+        /*self.notificationTaskTypes = []
+        
+        items.filter { $0.checked }.forEach { (item) in
+            if let taskType = TaskType(rawValue: item.identifier) {
+                notificationTaskTypes.append(taskType)
+            }
+        }
+        
+        if let data = try? JSONEncoder().encode(notificationTaskTypes) {
+            defaults.setValue(data, forKey: "notificationTaskTypes")
+        }*/
+    }
+    
+    func handleTaskPrioritiesPicker(items: [PickerItem]) {
+        
+    }
 }
 
 extension NotificationsPreferencesTableViewController {
@@ -91,5 +106,58 @@ extension NotificationsPreferencesTableViewController {
         }
         
         return normalCellHeight
+    }
+}
+
+// MARK: - Navigation
+extension NotificationsPreferencesTableViewController {
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        if let tasksFiltersController = segue.destination as? PickerTableViewController {
+            tasksFiltersController.identifier = segue.identifier
+            tasksFiltersController.unwindSegueIdentifier = "unwindNotificationsPreferences"
+            
+            if segue.identifier == "notificationTaskTypesSegue" {
+                tasksFiltersController.title = "Task types"
+                tasksFiltersController.multiSelect = true
+                
+                for taskType in TaskType.allCases {
+                    var pickerItem = PickerItem(identifier: taskType.rawValue, label: taskType.rawValue, checked: false)
+                    /*if notificationTaskTypes.contains(taskType) {
+                        pickerItem.checked = true
+                    }*/
+                    
+                    tasksFiltersController.items.append(pickerItem)
+                }
+            } else if segue.identifier == "notificationTaskPrioritiesSegue" {
+                tasksFiltersController.title = "Task priorities"
+                tasksFiltersController.multiSelect = true
+                
+                for taskPriority in TaskPriority.allCases {
+                    var pickerItem = PickerItem(identifier: taskPriority.rawValue, label: taskPriority.rawValue, checked: false)
+                    /*if notificationTaskTypes.contains(taskPriority) {
+                        pickerItem.checked = true
+                    }*/
+                    
+                    tasksFiltersController.items.append(pickerItem)
+                }
+            }
+        }
+    }
+    
+    @IBAction func unwindToNotificationsPreferences(_ unwindSegue: UIStoryboardSegue) {
+        let sourceViewController = unwindSegue.source
+        // Use data from the view controller which initiated the unwind segue
+        
+        if let pickerView = sourceViewController as? PickerTableViewController {
+            if unwindSegue.identifier == "notificationTaskTypesSegue" {
+                handleTaskTypesPicker(items: pickerView.items)
+            } else if unwindSegue.identifier == "notificationTaskPrioritiesSegue" {
+                handleTaskPrioritiesPicker(items: pickerView.items)
+            }
+        }
     }
 }
--- StudentTasks/Settings/SettingsTableViewController.swift
+++ StudentTasks/Settings/SettingsTableViewController.swift
@@ -12,9 +12,6 @@ class SettingsTableViewController: UITableViewController {
     
     // Notification section
     @IBOutlet var notificationSwitch: UISwitch!
-    @IBOutlet weak var notificationTasksLabel: UILabel!
-    
-    var notificationTaskTypes: [TaskType] = TaskType.allCases
     //
     
     override func viewDidLoad() {
@@ -34,62 +31,24 @@ class SettingsTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        
-        if let tasksFiltersController = segue.destination as? PickerTableViewController {
-            tasksFiltersController.unwindSegueIdentifier = "settingsUnwindSegue"
-            
-            if segue.identifier == "notificationTaskTypesSegue" {
-                tasksFiltersController.title = "Task types"
-                tasksFiltersController.identifier = "notificationTaskTypes"
-                tasksFiltersController.multiSelect = true
-                
-                for taskType in TaskType.allCases {
-                    var pickerItem = PickerItem(identifier: taskType.rawValue, label: taskType.rawValue, checked: false)
-                    if notificationTaskTypes.contains(taskType) {
-                        pickerItem.checked = true
-                    }
-                    
-                    tasksFiltersController.items.append(pickerItem)
-                }
-            }
-        }
     }
     
     @IBAction func unwindToSettingsTab(_ unwindSegue: UIStoryboardSegue) {
-        let sourceViewController = unwindSegue.source
+        // let sourceViewController = unwindSegue.source
         // Use data from the view controller which initiated the unwind segue
-        
-        if unwindSegue.identifier == "settingsUnwindSegue",
-           let pickerView = sourceViewController as? PickerTableViewController {
-            handleTaskTypePicker(items: pickerView.items)
-        }
     }
 }
 
 extension SettingsTableViewController {
     private func loadNotificationsSettings() {
         notificationSwitch.isOn = defaults.bool(forKey: "notificationEnabled")
-        if let data = UserDefaults.standard.data(forKey: "notificationTaskTypes"),
+        /*if let data = UserDefaults.standard.data(forKey: "notificationTaskTypes"),
            let types = try? JSONDecoder().decode(Array<TaskType>.self, from: data) {
             self.notificationTaskTypes = types
-        }
+        }*/
     }
     
     @IBAction func notificationSwitchChanged(_ sender: UISwitch) {
         defaults.setValue(sender.isOn, forKey: "notificationEnabled")
-    }
-    
-    private func handleTaskTypePicker(items: [PickerItem]) {
-        self.notificationTaskTypes = []
-        
-        items.filter { $0.checked }.forEach { (item) in
-            if let taskType = TaskType(rawValue: item.identifier) {
-                notificationTaskTypes.append(taskType)
-            }
-        }
-        
-        if let data = try? JSONEncoder().encode(notificationTaskTypes) {
-            defaults.setValue(data, forKey: "notificationTaskTypes")
-        }
     }
 }
