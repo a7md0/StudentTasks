c9ec50d69e006e362bd1b9f0ffd28e750d6338f4
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-30 09:55:45 +0300

    Use notification settings and reflect data in notification pref view

--- StudentTasks/Settings/NotificationsPreferencesTableViewController.swift
+++ StudentTasks/Settings/NotificationsPreferencesTableViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class NotificationsPreferencesTableViewController: UITableViewController {
 
+    var notificationSettings: NotificationSettings = NotificationSettings.load()
+    
+    @IBOutlet weak var taskTypesLabel: UILabel!
+    @IBOutlet weak var taskPrioritiesLabel: UILabel!
+    
     @IBOutlet weak var fromDateLabel: UILabel!
     @IBOutlet weak var fromDatePicker: UIDatePicker!
     var isFromDatePickerHidden = true
@@ -16,10 +21,6 @@ class NotificationsPreferencesTableViewController: UITableViewController {
     @IBOutlet weak var toDateLabel: UILabel!
     @IBOutlet weak var toDatePicker: UIDatePicker!
     var isToDatePickerHidden = true
-    
-    
-    @IBOutlet weak var taskTypesLabel: UILabel!
-    @IBOutlet weak var taskPrioritiesLabel: UILabel!
     
     let timeFormatter = DateFormatter()
     
@@ -34,12 +35,31 @@ class NotificationsPreferencesTableViewController: UITableViewController {
         timeFormatter.dateStyle = .none
         timeFormatter.timeStyle = .short
         
-        updateDateLabels()
-        updateTasksFilters()
+        updateView()
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIBarButtonItem) {
+        notificationSettings = NotificationSettings.reset()
+        updateView()
     }
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
         updateDateLabels()
+        
+        notificationSettings.preferredTimeRange.start = fromDatePicker.date
+        notificationSettings.preferredTimeRange.end = toDatePicker.date
+        notificationSettings.save()
+    }
+    
+    func updateView() {
+        loadSettings()
+        updateDateLabels()
+        updateTasksFilters()
+    }
+    
+    func loadSettings() {
+        fromDatePicker.date = notificationSettings.preferredTimeRange.start
+        toDatePicker.date = notificationSettings.preferredTimeRange.end
     }
     
     func updateDateLabels() {
@@ -48,25 +68,37 @@ class NotificationsPreferencesTableViewController: UITableViewController {
     }
     
     func updateTasksFilters() {
-        
+        taskTypesLabel.text = selectionString(notificationSettings.preferredTypes, with: TaskType.allCases)
+        taskPrioritiesLabel.text = selectionString(notificationSettings.preferredPriorities, with: TaskPriority.allCases)
+    }
+    
+    func selectionString(_ compare: Array<Any>, with: Array<Any>) -> String {
+        switch compare.count {
+        case 0:
+            return "None"
+        case with.count:
+            return "All"
+        default:
+            return "Custom"
+        }
     }
     
     func handleTaskTypesPicker(items: [PickerItem]) {
-        /*self.notificationTaskTypes = []
+        notificationSettings.preferredTypes = items.compactMap({ (item) -> TaskType? in
+            return item.checked ? TaskType(rawValue: item.identifier) : nil
+        })
         
-        items.filter { $0.checked }.forEach { (item) in
-            if let taskType = TaskType(rawValue: item.identifier) {
-                notificationTaskTypes.append(taskType)
-            }
-        }
-        
-        if let data = try? JSONEncoder().encode(notificationTaskTypes) {
-            defaults.setValue(data, forKey: "notificationTaskTypes")
-        }*/
+        notificationSettings.save()
+        updateTasksFilters()
     }
     
     func handleTaskPrioritiesPicker(items: [PickerItem]) {
+        notificationSettings.preferredPriorities = items.compactMap({ (item) -> TaskPriority? in
+            return item.checked ? TaskPriority(rawValue: item.identifier) : nil
+        })
         
+        notificationSettings.save()
+        updateTasksFilters()
     }
 }
 
@@ -116,33 +148,33 @@ extension NotificationsPreferencesTableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         
-        if let tasksFiltersController = segue.destination as? PickerTableViewController {
-            tasksFiltersController.identifier = segue.identifier
-            tasksFiltersController.unwindSegueIdentifier = "unwindNotificationsPreferences"
+        if let pickerTableView = segue.destination as? PickerTableViewController {
+            pickerTableView.identifier = segue.identifier
+            pickerTableView.unwindSegueIdentifier = "unwindNotificationsPreferences"
             
             if segue.identifier == "notificationTaskTypesSegue" {
-                tasksFiltersController.title = "Task types"
-                tasksFiltersController.multiSelect = true
+                pickerTableView.title = "Task types"
+                pickerTableView.multiSelect = true
                 
                 for taskType in TaskType.allCases {
                     var pickerItem = PickerItem(identifier: taskType.rawValue, label: taskType.rawValue, checked: false)
-                    /*if notificationTaskTypes.contains(taskType) {
+                    if notificationSettings.preferredTypes.contains(taskType) {
                         pickerItem.checked = true
-                    }*/
+                    }
                     
-                    tasksFiltersController.items.append(pickerItem)
+                    pickerTableView.items.append(pickerItem)
                 }
             } else if segue.identifier == "notificationTaskPrioritiesSegue" {
-                tasksFiltersController.title = "Task priorities"
-                tasksFiltersController.multiSelect = true
+                pickerTableView.title = "Task priorities"
+                pickerTableView.multiSelect = true
                 
                 for taskPriority in TaskPriority.allCases {
                     var pickerItem = PickerItem(identifier: taskPriority.rawValue, label: taskPriority.rawValue, checked: false)
-                    /*if notificationTaskTypes.contains(taskPriority) {
+                    if notificationSettings.preferredPriorities.contains(taskPriority) {
                         pickerItem.checked = true
-                    }*/
+                    }
                     
-                    tasksFiltersController.items.append(pickerItem)
+                    pickerTableView.items.append(pickerItem)
                 }
             }
         }
@@ -152,10 +184,11 @@ extension NotificationsPreferencesTableViewController {
         let sourceViewController = unwindSegue.source
         // Use data from the view controller which initiated the unwind segue
         
-        if let pickerView = sourceViewController as? PickerTableViewController {
-            if unwindSegue.identifier == "notificationTaskTypesSegue" {
+        if unwindSegue.identifier == "unwindNotificationsPreferences",
+           let pickerView = sourceViewController as? PickerTableViewController {
+            if pickerView.identifier == "notificationTaskTypesSegue" {
                 handleTaskTypesPicker(items: pickerView.items)
-            } else if unwindSegue.identifier == "notificationTaskPrioritiesSegue" {
+            } else if pickerView.identifier == "notificationTaskPrioritiesSegue" {
                 handleTaskPrioritiesPicker(items: pickerView.items)
             }
         }
