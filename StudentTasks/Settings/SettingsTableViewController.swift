334d6e55ef712f716f0d978b649f4462b9f51b97
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-30 10:30:24 +0300

    Implement gpa model selection in settings (in-progress)

--- StudentTasks/Settings/SettingsTableViewController.swift
+++ StudentTasks/Settings/SettingsTableViewController.swift
@@ -7,11 +7,31 @@
 
 import UIKit
 
+enum GpaModel: String, CustomStringConvertible, Codable, CaseIterable {
+    var description: String {
+        switch self {
+        case .fourPlus:
+            return "4.0 Scale (+)"
+        case .fourPlusMinus:
+            return "4.0 Scale (+/-)"
+        case .hundredPercentage:
+            return "Percentage scale (%)"
+        }
+    }
+    
+    case fourPlus = "4+", fourPlusMinus = "4+-", hundredPercentage = "100%"
+}
+
 class SettingsTableViewController: UITableViewController {
     private let defaults = UserDefaults.standard
     
     // Notification section
     @IBOutlet var notificationSwitch: UISwitch!
+    //
+    
+    // Grading section
+    var gpaModel: GpaModel = .fourPlusMinus
+    @IBOutlet weak var gpaModelLabel: UILabel!
     //
     
     override func viewDidLoad() {
@@ -25,17 +45,54 @@ class SettingsTableViewController: UITableViewController {
         loadNotificationsSettings()
     }
 
-    // MARK: - Navigation
+    func handleGpaModelPicker(items: [PickerItem]) {
+        guard let choosedItem = items.first(where: { $0.checked }),
+              let model = GpaModel(rawValue: choosedItem.identifier) else { return }
+        
+        gpaModel = model
+        gpaModelLabel.text = gpaModel.description
+        // TODO: Save choosed gpa model
+    }
+}
 
+// MARK: - Navigation
+extension SettingsTableViewController {
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        
+        if let pickerTableView = segue.destination as? PickerTableViewController {
+            pickerTableView.identifier = segue.identifier
+            pickerTableView.unwindSegueIdentifier = "unwindSettings"
+            
+            if segue.identifier == "gpaModelSegue" {
+                pickerTableView.title = "GPA Model"
+                pickerTableView.multiSelect = false
+                
+                for model in GpaModel.allCases {
+                    var pickerItem = PickerItem(identifier: model.rawValue, label: model.description, checked: false)
+                    /*if notificationSettings.preferredTypes.contains(model) {
+                        pickerItem.checked = true
+                    }*/
+                    
+                    pickerTableView.items.append(pickerItem)
+                }
+            }
+        }
     }
     
     @IBAction func unwindToSettingsTab(_ unwindSegue: UIStoryboardSegue) {
-        // let sourceViewController = unwindSegue.source
+        let sourceViewController = unwindSegue.source
         // Use data from the view controller which initiated the unwind segue
+        
+        if unwindSegue.identifier == "unwindSettings",
+           let pickerView = sourceViewController as? PickerTableViewController {
+            if pickerView.identifier == "gpaModelSegue" {
+                handleGpaModelPicker(items: pickerView.items)
+            }
+        }
     }
 }
 
