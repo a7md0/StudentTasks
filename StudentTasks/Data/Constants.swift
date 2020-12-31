c615d1659e5ff941ddafdea0d8755caaa07b0846
Ahmed Naser <201700099@student.polytechnic.bh>
2020-12-31 04:48:16 +0300

    Constant computed prop -> lazy

--- StudentTasks/Data/Constants.swift
+++ StudentTasks/Data/Constants.swift
@@ -10,21 +10,20 @@ import Foundation
 struct Constants {
     static let placeHolderDate = Date(timeIntervalSinceReferenceDate: -39139200) // Oct 6, 1999, 00:00 AM
     
-    static var timeFormatter: DateFormatter {
-        get {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "h:mm a"
-            
-            return formatter
-        }
-    }
+    static var timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        
+        return formatter
+    }()
     
-    static var notifcationFromTime: Date {
+    static var notifcationFromTime: Date = {
         return Constants.timeFormatter.date(from: "8:00 AM")!
-    }
-    static var notifcationToTime: Date  {
+    }()
+    
+    static var notifcationToTime: Date = {
         return Constants.timeFormatter.date(from: "10:00 PM")!
-    }
+    }()
     
     static let coursesNotifcations: [String : NSNotification.Name] = [
         "created": NSNotification.Name(rawValue: "CourseCreated"),
