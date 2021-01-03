86e6f73a75addf5ad3dc7af38e9cf823f4434cac
Ahmed Naser <201700099@student.polytechnic.bh>
2021-01-03 05:18:27 +0300

    Refactor calendar code

--- StudentTasks/Calendar/CalendarViewController.swift
+++ StudentTasks/Calendar/CalendarViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import FSCalendar
 
-class CalendarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+class CalendarViewController: UIViewController
 {
     var taskByDate: [Task] = []
     var ignoreNextUpdate: Bool = false
@@ -16,80 +16,8 @@ class CalendarViewController: UIViewController, UITableViewDelegate, UITableView
     var uniqueDate: [String] = []
  //   var dateTasks : [Task]?
  //   var Tasks: Task?
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return taskByDate.count
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let dateTask = taskByDate[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellIdentifier") as! TasksTableViewCell
 
-        // Configure the cell...
-        cell.taskLabel.text = dateTask.name
-        cell.subtitle.text = dateTask.brief
-
-        if let codableColor = dateTask.course?.color {
-            cell.courseImage.backgroundColor = codableColor.color
-        }
-        
-        return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "DateTaskSegue", sender: self)
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = segue.destination as? TaskDetailsTableViewController {
-            destination.tasks = taskByDate[(taskTableView.indexPathForSelectedRow?.row)!]
-    }
-    }
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let complete = UIContextualAction(style: .normal, title: "Complete") { (action, view, completionHandler) in
-            print("Complete \(indexPath.row + 1)")
-            
-            self.completeItem(indexPath: indexPath)
-        }
-        complete.backgroundColor = .gray
-        complete.image = UIImage(systemName: "checkmark")
-        
-        let delete = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
-            let task = self.taskByDate[indexPath.row]
-            
-            self.showConfirmDelete(task.name) { (delete) in
-                completionHandler(true)
-                
-                if delete {
-                    print("Delete \(indexPath.row + 1)")
-                
-                    self.deleteItem(indexPath: indexPath)
-                }
-            }
-        }
-        delete.image = UIImage(systemName: "trash")
-        
-        let swipe = UISwipeActionsConfiguration(actions: [complete, delete])
-        
-        return swipe
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return CGFloat(72)
-    }
-    
-    func showConfirmDelete(_ what: String, handler: ((Bool) -> Void)?) {
-        let confirmAlert = UIAlertController(title: "Delete \"\(what)\"?", message: "Deleting this task will delete all related data.", preferredStyle: UIAlertController.Style.alert)
-        
-        let okAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
-            handler?(true)
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
-            handler?(false)
-        }
-        
-        confirmAlert.addAction(okAction)
-        confirmAlert.addAction(cancelAction)
-
-        present(confirmAlert, animated: true, completion: nil)
-    }
-    
     let Formatter = DateFormatter()
     @IBOutlet weak var calendarView: FSCalendar!
     @IBOutlet weak var taskTableView: UITableView!
@@ -112,35 +40,14 @@ class CalendarViewController: UIViewController, UITableViewDelegate, UITableView
         print(datesWithEvent)
         print (uniqueDate)
     }
-    
 
     // MARK: - Navigation
-    
-    func completeItem(indexPath: IndexPath) {
-        var task = taskByDate[indexPath.row]
-        
-        task.complete(completedOn: nil)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? TaskDetailsTableViewController {
+            destination.tasks = taskByDate[(taskTableView.indexPathForSelectedRow?.row)!]
+        }
     }
-
-    func deleteItem(indexPath: IndexPath) {
-        var task = taskByDate[indexPath.row]
-        
-        taskByDate.remove(at: indexPath.row)
-        taskTableView.deleteRows(at: [indexPath], with: .left)
-        
-        ignoreNextUpdate = true
-        task.remove()
-    }
-
-
-
-    
-    
-    
 }
-
-
-
 
 
 extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegate {
@@ -190,3 +97,92 @@ extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegate {
     }
 }
     
+extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskByDate.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dateTask = taskByDate[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellIdentifier") as! TasksTableViewCell
+
+        // Configure the cell...
+        cell.taskLabel.text = dateTask.name
+        cell.subtitle.text = dateTask.brief
+
+        if let codableColor = dateTask.course?.color {
+            cell.courseImage.backgroundColor = codableColor.color
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "DateTaskSegue", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let complete = UIContextualAction(style: .normal, title: "Complete") { (action, view, completionHandler) in
+            print("Complete \(indexPath.row + 1)")
+            
+            self.completeItem(indexPath: indexPath)
+        }
+        complete.backgroundColor = .gray
+        complete.image = UIImage(systemName: "checkmark")
+        
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+            let task = self.taskByDate[indexPath.row]
+            
+            self.showConfirmDelete(task.name) { (delete) in
+                completionHandler(true)
+                
+                if delete {
+                    print("Delete \(indexPath.row + 1)")
+                
+                    self.deleteItem(indexPath: indexPath)
+                }
+            }
+        }
+        delete.image = UIImage(systemName: "trash")
+        
+        let swipe = UISwipeActionsConfiguration(actions: [complete, delete])
+        
+        return swipe
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(72)
+    }
+    
+    func showConfirmDelete(_ what: String, handler: ((Bool) -> Void)?) {
+        let confirmAlert = UIAlertController(title: "Delete \"\(what)\"?", message: "Deleting this task will delete all related data.", preferredStyle: UIAlertController.Style.alert)
+        
+        let okAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+            handler?(true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            handler?(false)
+        }
+        
+        confirmAlert.addAction(okAction)
+        confirmAlert.addAction(cancelAction)
+
+        present(confirmAlert, animated: true, completion: nil)
+    }
+    
+    func completeItem(indexPath: IndexPath) {
+        var task = taskByDate[indexPath.row]
+        
+        task.complete(completedOn: nil)
+    }
+
+    func deleteItem(indexPath: IndexPath) {
+        var task = taskByDate[indexPath.row]
+        
+        taskByDate.remove(at: indexPath.row)
+        taskTableView.deleteRows(at: [indexPath], with: .left)
+        
+        ignoreNextUpdate = true
+        task.remove()
+    }
+}
